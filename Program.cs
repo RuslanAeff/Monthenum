45f84946a7a5303d8eb00f421e4767ba11f66ac5
Aliyev Ruslan <ruslanra@code.local>
2023-07-04 18:54:49 +0400

    enum first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Months
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Year.January);
+        
+        }
+  
+    
+    }
+
+
+    enum Year
+    {
+        January=1,
+        Feburary,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+
+    
+    }
+
+   
+}
